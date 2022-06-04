c8111304df7e77a2de3935ac9c534d56d75ab9c7
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-06-04 11:26:28 +0300

    Adding postgres & auth

---
+++ blog/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from blog.models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Article
+        fields = ('title', 'date_created', 'updated',
+                  'author', 'content', 'published')
